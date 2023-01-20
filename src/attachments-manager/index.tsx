af779ed4fcfaf82e91156ab1833ec2f706e640b9
Lorenzo Bonatti <imlorenzobonatti@gmail.com>
2023-01-21 00:05:35 +0100

    feat: define minimal component props

--- src/attachments-manager/index.tsx
+++ src/attachments-manager/index.tsx
@@ -1,6 +1,44 @@
 import React, {ReactElement} from "react";
+import {Attachment, AttachmentManagerLayout, AttachmentManagerType} from "./models";
 
-export const AttachmentsManager = (): ReactElement => {
+interface AttachmentState extends Attachment {
+    /** The file binary for new attachments */
+    file?: File;
+    /** Flag for attachment to delete */
+    toDelete?: boolean;
+}
+
+interface onSaveProps {
+    /** Attachments without changes */
+    attachments: Attachment[];
+    /** Attachments to add */
+    added: AttachmentState[];
+    /** Attachments to delete */
+    deleted: AttachmentState[];
+}
+
+interface AttachmentsManagerProps {
+    source: Attachment[];
+    showPreview?: boolean;
+    showFileName?: boolean;
+    layout?: AttachmentManagerLayout;
+    type?: AttachmentManagerType;
+    onSave: (data: onSaveProps) => Promise<void>;
+}
+
+export const AttachmentsManager = (
+    {
+        source,
+        showPreview = true,
+        showFileName,
+        layout = AttachmentManagerLayout.List,
+        type = AttachmentManagerType.Confirm,
+        onSave
+    }: AttachmentsManagerProps): ReactElement => {
+
+    // State
+    const [attachments, setAttachments] = React.useState<AttachmentState[]>(source);
+
     return (
         <></>
     )
